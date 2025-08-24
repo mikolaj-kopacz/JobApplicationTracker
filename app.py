ed1027f6c3c42fbf388e80e3e12fa9d5af19ca9e
mikolaj-kopacz <mikolajk411@gmail.com>
2025-08-25 00:18:28 +0200

    Add favicon, login manager, change navbar background from transparent to color when scrolled, add index elements

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
-
+from flask_login import LoginManager
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
 
 @app.route('/')
 def index():
